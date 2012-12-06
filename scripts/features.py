9c7973567d23cae82eb283d7277bb44c7fd3baf1
Jie <chen.ji7@gmail.com>
2012-12-06 03:03:04 -0500

    simple change, mostly to test new url setting for repo clone

--- scripts/features.py
+++ scripts/features.py
@@ -1,9 +1,21 @@
 # compute features for bioharness data
 
-import sys, numpy
+import sys, numpy, scipy
 from math import sqrt
 
 WINDOW_SIZE = 30
 
-def std(data):
-    return numpy.std(data)
+def compute_features(data):
+    feature_vector = []
+    feautre_name = []
+
+    # add the standard deviation
+    feature_vector.append(numpy.std(data))
+    feature_name.append('std')
+
+    # add the mean
+    feature_vector.append(scipy.mean(data))
+    feature_vector.append('mean')
+
+    
+
